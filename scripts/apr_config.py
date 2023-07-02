c532ca6401f3229302a1c783496e90cb40b16c96
Asichurter <asichurter@gmail.com>
2023-07-02 12:59:19 +0800

    update reproduce config & add mining config

--- scripts/apr_config.py
+++ scripts/apr_config.py
@@ -1,12 +1,37 @@
 
 
-config = {
-    'checkstyle_checkstyle': {
-        'repo_path': '/root/data/GHRB/repos/checkstyle/',
+reproduce_config = {
+    'checkstyle': {
+        'repo_path': '/home/user/projects/checkstyle/',
         'src_dir': 'src/main/java/',
         'test_prefix': 'src/test/java/',
-        'project_name': 'checkstyle_checkstyle',
+        'project_name': 'checkstyle',
         'project_id': 'checkstyle',
+        'timeout': '3m',
         'extra_test_config': []
+    }
+}
+
+bug_mining_config = {
+    'checkstyle': {
+        "repo_path": "/home/user/projects/checkstyle/",
+        "size_limit": {
+            "not_test_file": 1,
+            "not_test_hunk": 3,
+            "not_test_func": 3,
+            "not_test_loc": 30,
+        },
+        "reproduction": {
+            "pre_fix_ver": {
+                "run_succeed": True,
+                "test_passed": None     # None refer to "not limit"
+            },
+            "post_fix_ver": {
+                "run_succeed": True,
+                "test_passed": True
+            },
+            'timeout': '3m',
+            'extra_test_config': []
+        }
     }
 }
