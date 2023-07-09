f8a26642ea1c01521e8d1cd6b2b0aa51a208f622
Asichurter <asichurter@gmail.com>
2023-07-10 00:58:26 +0800

    fix special method name pattern

--- scripts/apr_bug_mine_re.py
+++ scripts/apr_bug_mine_re.py
@@ -1,8 +1,8 @@
 import re
 
-failed_file_pattern = re.compile("\[ERROR\] Tests run: [0-9]*, Failures: [0-9]*, Errors: [0-9]*, Skipped: [0-9]*, Time elapsed: .* s <<< FAILURE! - in ([a-zA-Z.]*)")
-failure_method_pattern = re.compile("\[ERROR\] ([a-zA-Z]*)  Time elapsed: [0-9\.]* s  <<< FAILURE\!")
-error_method_pattern = re.compile("\[ERROR\] ([a-zA-Z]*)  Time elapsed: [0-9\.]* s  <<< ERROR\!")
+failed_file_pattern = re.compile("\[ERROR\] Tests run: [0-9]*, Failures: [0-9]*, Errors: [0-9]*, Skipped: [0-9]*, Time elapsed: .* s <<< FAILURE! - in ([a-zA-Z._\{\}]*)")
+failure_method_pattern = re.compile("\[ERROR\] ([0-9a-zA-Z_\{\}]*)  Time elapsed: [0-9\.]* s  <<< FAILURE\!")
+error_method_pattern = re.compile("\[ERROR\] ([0-9a-zA-Z_\{\}]*)  Time elapsed: [0-9\.]* s  <<< ERROR\!")
 
 def extract_failed_file(line: str):
     return re.findall(failed_file_pattern, line)
@@ -14,5 +14,6 @@ def extract_error_method(line: str):
     return re.findall(error_method_pattern, line)
 
 if __name__ == "__main__":
-    line = "[ERROR] testInlineForbidden  Time elapsed: 0.045 s  <<< FAILURE!"
-    print(extract_failure_method(line))
+    line = "[ERROR] testArrayDeclarations3  Time elapsed: 0.001 s  <<< FAILURE!"
+    # print(extract_failure_method(line))
+    print(extract_error_method(line))
