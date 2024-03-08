ffdd326730d8ece9bdbdaaf3e0b7a158010b05b8
Asichurter <asichurter@gmail.com>
2024-03-08 15:54:03 +0800

    update docker start script

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-containername="libro-exp"
+containername="asichurter-libro"
 
 docker ps -aq --filter "name=$containername" | grep -q . && docker stop $containername && docker rm $containername
-docker run -dt --name $containername --env-file ./env.list -v $(pwd)/data:/root/data -v $(pwd)/scripts:/root/scripts -v $(pwd)/results:/root/results greenmon/libro-env:latest 
+docker run -dt --name $containername --env-file ./env.list -v $(pwd)/data:/home/user/data -v $(pwd)/scripts:/home/user/scripts -v $(pwd)/results:/home/user/results greenmon/libro-env:latest
 docker exec -it $containername /bin/bash
